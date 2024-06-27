02b139b11d2f0a05a377ee158264367c344d0f37
ajsierra1977 <sistemas@metalnorhierros.com>
2024-06-27 15:03:53 -0300

    se agregaron dos botones uno en Buscar y otro en ABMClientes

--- src/app/ventanaABMCliente.js
+++ src/app/ventanaABMCliente.js
@@ -1,8 +1,11 @@
 document.addEventListener('DOMContentLoaded',function (){
 
     const Aceptar = document.getElementById('btnVClienteAceptar');
-    const VBuscar = document.getElementById('VentanaBuscar');
+    const Cancelar = document.getElementById('btnVClienteCancelar');
+    const Editar = document.getElementById('btnVClienteEditar');
+    
     const vAbmCliente= document.getElementById('abmClientes');
+    const VBuscar = document.getElementById('VentanaBuscar');
     
     if (Aceptar){
         Aceptar.addEventListener("click",()=>{
@@ -13,5 +16,16 @@ document.addEventListener('DOMContentLoaded',function (){
             VBuscar.style.display="block";
         })
     }
-
+    if (Cancelar){
+        Cancelar.addEventListener("click",()=>{
+            console.log('click en aceptar')
+            //Ocultar Ventana abmClientes
+            vAbmCliente.style.display="none";
+            //Mostrar ventana Buscar
+            VBuscar.style.display="block";
+        })
+    }
+    if (Editar){
+        //Habilitar todos los controles
+    }
 })
